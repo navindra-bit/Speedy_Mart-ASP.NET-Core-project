57fc90003932ebe92ca8eb620acd64e9934d73dd
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-06-26 23:08:33 +0530

    controller update

--- Controllers/UserentryController.cs
+++ Controllers/UserentryController.cs
@@ -8,7 +8,7 @@ namespace Speedy_Groceries.Controllers
         [HttpGet]
         public IActionResult Login()
         {
-             
+            
             return View();
         }
         [HttpGet]
@@ -17,9 +17,13 @@ namespace Speedy_Groceries.Controllers
             UserInfo user = new UserInfo();
             return View(user);
         }
-        [HttpPut]
+       
         public IActionResult Userdata(UserInfo userdata)
         {
+             string name =userdata.name;
+             string email = userdata.email;
+            string password = userdata.password;
+
             SqlConnection sqlConnection = new SqlConnection("Data Source=NAVINDRA-M\\SQLEXPRESS; Initial Catalog = EwebUserInfo; Integrated security=true; encrypt = false");
             sqlConnection.Open();
             string sql = "insert into userdata (name,email,password,address,phoneNumber) values(@name,@email,@password,@address,@phoneNumber)";
@@ -27,15 +31,41 @@ namespace Speedy_Groceries.Controllers
             command.Parameters.AddWithValue("@name",userdata.name);
             command.Parameters.AddWithValue("@email", userdata.email);
             command.Parameters.AddWithValue("@password", userdata.password);
-            command.Parameters.AddWithValue("@address", userdata.address);
-            command.Parameters.AddWithValue("@phoneNumber", userdata.phoneNumber);
+            command.Parameters.AddWithValue("@address",string.IsNullOrEmpty( userdata.address) ? DBNull.Value : userdata.address);
+            command.Parameters.AddWithValue("@phoneNumber", userdata.phoneNumber.HasValue ? userdata.phoneNumber.Value : DBNull.Value);
             int value = command.ExecuteNonQuery();
             sqlConnection.Close();
             if (value > 0)
             {
                 return View("Login");
             }
+             
+            
             return NoContent();
+        }
+        public  IActionResult UserLog(UserInfo userdata)
+        {
+
+            using (SqlConnection sqlConnection = new SqlConnection("Data Source=NAVINDRA-M\\SQLEXPRESS; Initial Catalog = EwebUserInfo; Integrated security=true; encrypt = false"))
+            {
+                sqlConnection.Open();
+                string sql = "select email, password from userdata where email= @email and password= @password";
+                SqlCommand command = new SqlCommand(sql, sqlConnection);
+
+                command.Parameters.AddWithValue("@email", userdata.email);
+                command.Parameters.AddWithValue("@password", userdata.password);
+
+                SqlDataReader reader = command.ExecuteReader();
+                 
+                if (reader.HasRows)
+                {
+                    reader.Close();
+                    return RedirectToAction("Index", "Home");
+                }
+                reader.Close();
+            }
+            ViewBag.Message = "Invalid email or password";
+            return View("Login");
         }
     }
 }
