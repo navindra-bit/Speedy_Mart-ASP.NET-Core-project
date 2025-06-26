17cb7cfa2c7b3df16288d5d036891ead6a41ec28
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-06-26 23:06:36 +0530

    SpeedyMartDB Query

---
+++ _MSSQL Query/SpeedyMartDB.sql
@@ -0,0 +1,16 @@
+create database SpeedyMartDB
+
+use SpeedyMartDB
+
+create table  UserInfo  ( UserId INT identity(1,1) Primary Key,
+FullName nvarchar(100) not null ,
+Email nvarchar(100) unique not null ,
+Password nvarchar(100) not null,
+Gender Nvarchar(10) ,
+DateOfBirth Date ,
+PhoneNumber Bigint ,
+ProfileImage Nvarchar(300),
+CreatedAt datetime default getdate())
+
+ 
+select * from UserInfo
