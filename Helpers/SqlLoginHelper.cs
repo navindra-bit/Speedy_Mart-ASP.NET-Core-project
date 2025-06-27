cdfa9b131544e13769f993193448b48a72f729c1
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-06-27 23:21:21 +0530

    SqlLoginHelper

---
+++ Helpers/SqlLoginHelper.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Data.SqlClient;
+using Speedy_Groceries.Models;
+ 
+namespace Speedy_Groceries.Helpers
+{
+    public class SqlLoginHelper
+    {
+
+        public static bool Logcheck(UserInfo info)
+        {
+            string password = PasswordHelper.HashPassword(info.password);
+            using (SqlConnection sqlConnection = new SqlConnection("Data Source=NAVINDRA-M\\SQLEXPRESS; Initial Catalog = EwebUserInfo; Integrated security=true; encrypt = false"))
+            {
+                sqlConnection.Open();
+                string sql = "select 1 from userdata where email= @email and password= @password";
+                SqlCommand command = new SqlCommand(sql, sqlConnection);
+
+                command.Parameters.AddWithValue("@email", info.email);
+                command.Parameters.AddWithValue("@password", password);
+
+                 var IsVaildLog = command.ExecuteScalar();
+                return IsVaildLog!=null;
+            }
+             
+        }
+    }
+}
