d85aca8a9fa6087d4aeb2e3ebbd54d5216fdc962
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-06-27 23:20:40 +0530

    Passwordhash helper

---
+++ Helpers/PasswordHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace Speedy_Groceries.Helpers
+{
+    public class PasswordHelper
+    {
+        public static string HashPassword(string password)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                byte[] inputBytes = Encoding.UTF8.GetBytes(password);
+                byte[] hashBytes = sha256.ComputeHash(inputBytes);
+
+                 
+                StringBuilder sb = new StringBuilder();
+                foreach (var b in hashBytes)
+                    sb.Append(b.ToString("x2"));
+
+                return sb.ToString();
+            }
+        }
+    }
+}
