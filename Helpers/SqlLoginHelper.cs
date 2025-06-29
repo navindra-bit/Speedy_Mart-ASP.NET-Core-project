fbd160e8ca326a0b5df37634ffcb9e8af7449b37
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-06-29 23:14:52 +0530

    sqlLogHelper update

--- Helpers/SqlLoginHelper.cs
+++ Helpers/SqlLoginHelper.cs
@@ -6,20 +6,27 @@ namespace Speedy_Groceries.Helpers
     public class SqlLoginHelper
     {
 
-        public static bool Logcheck(UserInfo info)
+        public static (bool,string?) Logcheck(UserInfo info)
         {
             string password = PasswordHelper.HashPassword(info.password!);
             using (SqlConnection sqlConnection = new SqlConnection("Data Source=NAVINDRA-M\\SQLEXPRESS; Initial Catalog = EwebUserInfo; Integrated security=true; encrypt = false"))
             {
                 sqlConnection.Open();
-                string sql = "select 1 from userdata where email= @email and password= @password";
+                string sql = "select * from userdata where email= @email and password= @password";
                 SqlCommand command = new SqlCommand(sql, sqlConnection);
 
                 command.Parameters.AddWithValue("@email", info.email);
                 command.Parameters.AddWithValue("@password", password);
 
-                 var IsVaildLog = command.ExecuteScalar();
-                return IsVaildLog!=null;
+                SqlDataReader dataReader = command.ExecuteReader();
+
+                if (dataReader.Read())
+                {
+
+                    string? name = dataReader["name"] != DBNull.Value ? dataReader["name"].ToString() : "UserName";
+                    return (true, name);
+                }
+                return (false, string.Empty);
             }
              
         }
