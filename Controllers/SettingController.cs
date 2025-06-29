3cc07749ebf2d97f67f368a27a39638171288bdb
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-06-29 23:15:25 +0530

    Controller update

---
+++ Controllers/SettingController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Speedy_Groceries.Controllers
+{
+    public class SettingController : Controller
+    {
+        public IActionResult UserProfile()
+        {
+            return View();
+        }
+        public IActionResult Help()
+        {
+            return View();
+        }
+    }
+}
