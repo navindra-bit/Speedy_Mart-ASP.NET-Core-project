6d1f7a39a5e457bad4081ca0599fd64f752e942e
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-07-02 21:11:27 +0530

    Model update

--- Models/UserInfo.cs
+++ Models/UserInfo.cs
@@ -1,13 +1,21 @@
-﻿namespace Speedy_Groceries.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Speedy_Groceries.Models
 {
     public class UserInfo
     {
         public int? uid {  get; set; }
+        [DataType(DataType.Text)]
         public string? name { get; set; }
+        [DataType(DataType.Password)]
         public string? password { get; set; }
+        [DataType(DataType.Password)]
         public string? Confirmpassword { get; set; }
+        [DataType(DataType.EmailAddress)]
         public string? email { get; set; }
+        [DataType(DataType.MultilineText)]
         public string? address { get; set; }
+        [DataType(DataType.PhoneNumber)]
         public long? phoneNumber { get; set; }
     }
 }
