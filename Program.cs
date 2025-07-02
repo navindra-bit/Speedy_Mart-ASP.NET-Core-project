67d66d0e7e8216629d14da2c469be43e50459b58
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-07-02 21:22:35 +0530

    Route update

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ builder.Services.AddControllersWithViews();
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
+app.UseRouting();
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
