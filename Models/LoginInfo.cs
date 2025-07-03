aea88c6e1b38e6dd8cee7db5453b617c13a6e551
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-07-03 20:07:04 +0530

    Login model

---
+++ Models/LoginInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace Speedy_Groceries.Models
+{
+    public class LoginInfo
+    {
+        [ValidateNever]
+        public int? loginuid { get; set; }
+
+
+        [Required(ErrorMessage = "*Please enter your email.")]
+        [EmailAddress(ErrorMessage = "*Invalid email address.")]
+        [DataType(DataType.EmailAddress)]
+        public string? Loginemail { get; set; }
+
+
+        [DataType(DataType.Password)]
+        [Required(ErrorMessage = "*Please enter the password.")]
+        
+        public string? Loginpassword { get; set; }
+    }
+}
