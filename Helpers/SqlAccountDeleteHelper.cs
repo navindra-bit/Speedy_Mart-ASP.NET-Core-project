79dfb8ae7855232f4d2e0314336d375e7020cc12
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-07-03 20:06:28 +0530

    helper add update

---
+++ Helpers/SqlAccountDeleteHelper.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Data.SqlClient;
+using Speedy_Groceries.Models;
+
+namespace Speedy_Groceries.Helpers
+{
+    public class SqlAccountDeleteHelper
+    {
+        //public static (bool, string?) AccDelete(UserInfo info)
+        //{
+        //    string password = PasswordHelper.HashPassword(info.password!);
+        //    using (SqlConnection sqlConnection = new SqlConnection("Data Source=NAVINDRA-M\\SQLEXPRESS; Initial Catalog = SpeedyMartDB; Integrated security=true; encrypt = false"))
+        //    {
+        //        sqlConnection.Open();
+        //        string sql = "DELETE FROM UserInfo WHERE Email = @email AND Password = @password";
+        //        SqlCommand command = new SqlCommand(sql, sqlConnection);
+
+        //        command.Parameters.AddWithValue("@email", info.email);
+        //        command.Parameters.AddWithValue("@password", password);
+
+        //        int value = command.ExecuteNonQuery();
+        //        if (value > 0)
+        //        {
+        //            return (true, "Deleted Account successful");
+        //        }
+        //        return (false, string.Empty);
+        //    }
+
+        //}
+        public static (bool, string?) AccDelete(string Userid)
+        {
+            
+            using (SqlConnection sqlConnection = new SqlConnection("Data Source=NAVINDRA-M\\SQLEXPRESS; Initial Catalog = SpeedyMartDB; Integrated security=true; encrypt = false"))
+            {
+                sqlConnection.Open();
+                string sql = "DELETE FROM UserInfo WHERE  UserId=@Userid";
+                SqlCommand command = new SqlCommand(sql, sqlConnection);
+
+                command.Parameters.AddWithValue("@Userid", Userid);
+                
+
+                int value = command.ExecuteNonQuery();
+                if (value > 0)
+                {
+                    return (true, "Deleted Account successful");
+                }
+                return (false, string.Empty);
+            }
+
+        }
+    }
+}
