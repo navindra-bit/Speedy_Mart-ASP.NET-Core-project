b1e2ccb3d6dc596880141b0776d2e484132251e0
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-07-03 20:04:59 +0530

    User Controller update

--- Controllers/UserentryController.cs
+++ Controllers/UserentryController.cs
@@ -2,6 +2,7 @@
 using Speedy_Groceries.Models;
 using Microsoft.Data.SqlClient;
 using Speedy_Groceries.Helpers;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 namespace Speedy_Groceries.Controllers
 {
     public class UserentryController : Controller
@@ -18,38 +19,66 @@ namespace Speedy_Groceries.Controllers
             UserInfo user = new UserInfo();
             return View(user);
         }
-       
+          //User Register
         public IActionResult Userdata(UserInfo userdata)
         {
-            ViewBag.AlertType = "danger";
-            if (userdata.password != userdata.Confirmpassword)
+
+            if (ModelState.IsValid )
             {
-                ViewBag.Message = "Oops! Passwords do not match. Please try again.";
-                return View("Register", userdata);
-            }
-            
-            var Reg = SqlSignupHelper.UserReg(userdata);
-             ViewBag.Message = Reg.message;
+                if(!SqlSignupHelper.UserNameExists(userdata)  )
+                {
+                    ModelState.AddModelError("name", "*Username already exists.");
+                    return View("Register", userdata);
+                }
+                if (!SqlSignupHelper.UserEmailExists(userdata))
+                {
+                    ModelState.AddModelError("email", "*Email already exists.");
+                    return View("Register", userdata);
+                }
+                if (!SqlSignupHelper.UserPhoneNumberExists(userdata))
+                {
+                    ModelState.AddModelError("phoneNumber", "*PhoneNumber already exists.");
+                    return View("Register", userdata);
+                }
+
+                var Reg = SqlSignupHelper.UserReg(userdata);
+                ViewBag.Message = Reg.message;
                 if (Reg.isvaild)
                 {
-                 ViewBag.AlertType = "success";
-                 return View("Register", userdata);
+                    ViewBag.AlertType = "success";
+                    return View("Register", userdata);
+                }
+                return View("Register", userdata);
+
+            }
+            return View("Register", userdata);
+        }
+           
+      //  user Login
+        public  IActionResult UserLog(LoginInfo loguserdata)
+        {
+            if (ModelState.IsValid)
+            {
+                if (SqlLoginHelper.UserEmailExists(loguserdata.Loginemail!))
+                {
+                    ModelState.AddModelError("Loginemail", "*Email does not exists.");
+                    return View("Login", loguserdata);
+                }
+                 
+                TempData["LoginAth"] = false;
+                var isvaild = SqlLoginHelper.Logcheck(loguserdata);
+                if(!isvaild.Item1 )
+                {
+                    ModelState.AddModelError("Loginpassword", "*Please enter a valid password.");
+                        return View("Login", loguserdata);
                 }
                
-                return View("Register", userdata);
+                    TempData["username"] = isvaild.Item2;
+                    TempData["LoginAth"] = true;
+                    TempData["Userid"] = isvaild.Item3;
+                    return RedirectToAction("Index", "Home");                 
+               
             }
-           
-        public  IActionResult UserLog(UserInfo userdata)
-        {
-            TempData["LoginAth"] = false;
-            var isvaild = SqlLoginHelper.Logcheck(userdata);
-            if (isvaild.Item1)
-             {
-                TempData["username"] = isvaild.Item2;
-              TempData["LoginAth"] = true;
-               return RedirectToAction("Index", "Home");
-             }   
-            ViewBag.Message = "Invalid email or password";
             return View("Login");
         }
 
