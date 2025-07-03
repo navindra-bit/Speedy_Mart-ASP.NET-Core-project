a476b6c65308a6b60307fc7a4edf4f98a142fcce
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-07-03 20:05:39 +0530

    helper update

--- Helpers/SqlLoginHelper.cs
+++ Helpers/SqlLoginHelper.cs
@@ -5,28 +5,49 @@ namespace Speedy_Groceries.Helpers
 {
     public class SqlLoginHelper
     {
-
-        public static (bool,string?) Logcheck(UserInfo info)
+        public static bool UserEmailExists(string userdata)
         {
-            string password = PasswordHelper.HashPassword(info.password!);
-            using (SqlConnection sqlConnection = new SqlConnection("Data Source=NAVINDRA-M\\SQLEXPRESS; Initial Catalog = EwebUserInfo; Integrated security=true; encrypt = false"))
+            string? email = userdata ;
+            using (SqlConnection sqlConnection = new SqlConnection("Data Source=NAVINDRA-M\\SQLEXPRESS; Initial Catalog = SpeedyMartDB; Integrated security=true; encrypt = false"))
             {
                 sqlConnection.Open();
-                string sql = "select * from userdata where email= @email and password= @password";
+                string sql2 = "Select 1 from UserInfo where Email=@email";
+                SqlCommand command = new SqlCommand(sql2, sqlConnection);
+
+                command.Parameters.AddWithValue("@email", userdata );
+
+                var value = command.ExecuteScalar();
+                if (value != null)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        public static (bool,string?,string?) Logcheck(LoginInfo info)
+        {
+
+
+            string password = info.Loginpassword!;
+            // string password = PasswordHelper.HashPassword(info.Loginpassword!); 
+            using (SqlConnection sqlConnection = new SqlConnection("Data Source=NAVINDRA-M\\SQLEXPRESS; Initial Catalog = SpeedyMartDB; Integrated security=true; encrypt = false"))
+            {
+                sqlConnection.Open();
+                string sql = "select * from UserInfo where Email= @email and Password= @password";
                 SqlCommand command = new SqlCommand(sql, sqlConnection);
 
-                command.Parameters.AddWithValue("@email", info.email);
+                command.Parameters.AddWithValue("@email", info.Loginemail);
                 command.Parameters.AddWithValue("@password", password);
 
                 SqlDataReader dataReader = command.ExecuteReader();
 
                 if (dataReader.Read())
                 {
-
-                    string? name = dataReader["name"] != DBNull.Value ? dataReader["name"].ToString() : "UserName";
-                    return (true, name);
+                    string?  Id = dataReader["UserId"] != DBNull.Value ? dataReader["UserId"].ToString() : "0";
+                    string? name = dataReader["FullName"] != DBNull.Value ? dataReader["FullName"].ToString() : "UserName";
+                    return (true, name,  Id);
                 }
-                return (false, string.Empty);
+                return (false, string.Empty,string.Empty);
             }
              
         }
