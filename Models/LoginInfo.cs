0c4fc6863c6552186130fdb59ca3ae658f814a43
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-07-04 21:30:08 +0530

    Model login info update

--- Models/LoginInfo.cs
+++ Models/LoginInfo.cs
@@ -8,13 +8,13 @@ namespace Speedy_Groceries.Models
         [ValidateNever]
         public int? loginuid { get; set; }
 
-
+        [Display(Name ="User Email")]
         [Required(ErrorMessage = "*Please enter your email.")]
         [EmailAddress(ErrorMessage = "*Invalid email address.")]
         [DataType(DataType.EmailAddress)]
         public string? Loginemail { get; set; }
 
-
+        [Display(Name ="Password")]
         [DataType(DataType.Password)]
         [Required(ErrorMessage = "*Please enter the password.")]
         
