f5d188843f2dd6c9a78f97cfebd26ea903a65400
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-07-10 22:52:51 +0530

    Program.cs update

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
 
+using Microsoft.EntityFrameworkCore;
+using Speedy_Groceries;
+using Speedy_Groceries.DataMethods;
+using Speedy_Groceries.Interface;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
+builder.Services.AddTransient<IMath,BasicMath>();
+//builder.Services.AddScoped<IMath, BasicMath>();
+//builder.Services.AddSingleton<IMath, BasicMath>();
+
+var SqlConnect = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDataBaseContext>(options => options.UseSqlServer(SqlConnect));
 var app = builder.Build();
 
 //app.MapGet("/", () => "Hello World!");
@@ -10,3 +21,5 @@ app.MapControllerRoute(
     pattern: "{controller=Home}/{action=Index}/{id?}");
 app.UseStaticFiles();
 app.Run();
+
+
