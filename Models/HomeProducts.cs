60e255a048e3fcb50bb56681b69dc286cd850ff3
Navindra M <84118384+navindra-bit@users.noreply.github.com>
2025-07-13 20:16:58 +0530

    Update HomeProducts.cs

--- Models/HomeProducts.cs
+++ Models/HomeProducts.cs
@@ -6,7 +6,7 @@
         public string? Name { get; set; }
         public string? Offer { get; set; }
         public string? Price { get; set; }
-        public string? Link { get; set; }
+        //public string? Link { get; set; }
 
     }
 }
